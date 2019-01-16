362edf956174842b0f8df260f833fd8351931442
AntoienM <33087066+AntoienM@users.noreply.github.com>
2019-01-16 14:13:19 +0100

    BDD

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,29 +6,31 @@
 
 <body>
         <div class="center">
-            <form action="login.php" method="POST">
+            <form action="session.php" method="POST">
                 <div class="imgcontainer">
-                    <img src="img_avatar.png" alt="Avatar" class="avatar">
-                </div>
-
-                <div class="container">
+                    <img src="assets/img/img_avatar.png" alt="Avatar" class="avatar">
+				</div>
+				<div>
                     <label for="login"><b>Nom d'utilisateur</b></label>
                     <input type="text" placeholder="Entrer votre nom d'utilisateur " name="login" required>
 
                     <label for="pwd"><b>Mot de passe</b></label>
                     <input type="password" placeholder="Entrer votre mot de passe" name="pwd" required>
 
-                    <button type="submit">Se connecter</button>
+                    <button type="submit" style="background-color:#09a02c;border:solid black 2px; ">Se connecter</button>
+                   
                     <label>
-                    <input type="checkbox" checked="checked" name="remember"> Se rappeler de moi
+                    	<input type="checkbox" checked="checked" name="remember"> Se rappeler de moi
                     </label>
                 </div>
 
-                <div class="container" style="background-color:#f1f1f1">
-                    <button type="button" class="cancelbtn">Annuler</button>
+                <div class="down-container" style="background-color:#f1f1f1; border-radius:15px;">
+                
+                    <button type="button" class="cancelbtn" id="back">Retour</button>
                     <span class="psw"><a href="#">Mot de passe oublié?</a></span>
+                    <button type="button" id="signin" >Pas encore membre ? S'inscrire</button>
                 </div>
             </form> 
         </div>
 
-    </body>
+	</body>
