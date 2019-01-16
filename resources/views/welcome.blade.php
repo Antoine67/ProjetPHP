cd694e8afbaaa56951e62e713d65341903af1379
smallval <33154705+smallval@users.noreply.github.com>
2019-01-16 14:54:53 +0100

    suite images acceuil

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,14 @@
 @extends('layout')
 
 @section('content')
+<link rel="stylesheet" href="{{ asset('/css/acceuil.css') }}">
 <p>
-    <img src="{{ asset('/img/1.jpeg') }}"> 
+    <img class="ligne1" src="{{ asset('/img/1.jpeg') }}" > 
+    <img class="ligne1" src="{{ asset('/img/2.png') }}"> 
+    <img class="ligne1" src="{{ asset('/img/3.jpg') }}"> 
+    <img class="ligne2" src="{{ asset('/img/3.jpg') }}"> 
+
+
+
 </p>
 @endsection
