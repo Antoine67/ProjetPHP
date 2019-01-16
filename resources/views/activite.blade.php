e2f51d4f863e5d429ca253a3a308efa261a0100b
Bada67 <guillaume.pires@viacesi.fr>
2019-01-16 16:30:17 +0100

    alignement image et changement couleur balise hr

--- resources/views/activite.blade.php
+++ resources/views/activite.blade.php
@@ -6,30 +6,50 @@
 
 </head>
 <div class="container-fluid text-center"> 
-         <div class="container-fluid text-center"> 
       <h1> Nos activités </h1>
-      <hr color="darkgrey">
+      <hr class="hr2">
       <div class="row">
         <div class="col-lg-4 col-md-5 col-sm-6">
-              <h2><br>Ford RANGER</h2>
-                     <p>Vos collègues, votre famille, vos amis... Le nouveau Ranger vous offre l'espace qu'il vous faut parmi trois styles de cabines et quatre modèles. 
-Toujours aussi spacieux et mieux équipé que jamais, vous et vos passagers profiterez d'un véritable espace de confort et d'élégance. </p>  
-                    <img id="accrobranche" src="{{ asset('/img/images.jpeg') }}" alt="Accrobranche" >
-                    <img id="footbal" src="{{ asset('/img/foot.png') }}" alt="Foot">
-                    <img id="badminton" src="{{ asset('/img/badminton.png') }}" alt="Badminton">
+            <div class="divv">
+                <h2><br>Accrobranche</h2>
+                    <p>Vos collègues, votre famille, vos amis... Le nouveau Ranger vous offre l'espace qu'il vous faut parmi trois styles de cabines et quatre modèles. 
+                    Toujours aussi spacieux et mieux équipé que jamais, vous et vos passagers profiterez d'un véritable espace de confort et d'élégance. 
+                    </p>  
+            </div>
+            
+            <div>
+                <img src="{{ asset('/img/foot.png') }}" alt="Image">
+            </div> 
         </div>
 
-          <div class="col-lg-4 col-md-5 col-sm-6">
-                  <h2><br>Ford GT</h2>
-                     <p>L'innovation. Voilà ce qui caractérise la Ford GT. Sa forme aérodynamique, ses renforts multifonction et son moteur V6 3,5 L EcoBoost® délivrant une puissance hors normes : tout dans la Ford GT est conçu pour la performance. </p> <br> <br> 
-                     <img id="ford gt" class="img-responsive" src="f:/ford gt.jpg" width="100% \9"  alt="Ford GT"> 
-          </div>
-          <div class="col-lg-4 col-md-5 col-sm-6">
-                    <h2> <br>Ford MUSTANG GT</h2>
-                     <p>Un nouveau design associant agressivité et efficacité aérodynamique.
-Des performances inégalées avec la suspension adaptative pilotée MagneRide™; un plaisir de conduite incomparable grâce au nouvel échappement actif, sublimant les vocalises du mythique V8 </p> <br>
-          <img id="mustang gt" class="img-responsive" src="f:/mustang gt 2.jpeg" width="100% \9" alt="Mustang">
+        <div class="col-lg-4 col-md-5 col-sm-6">
+            <div class="divv">
+                <h2><br>Football</h2>
+                    <p>L'innovation. Voilà ce qui caractérise la Ford GT. Sa forme aérodynamique, ses renforts multifonction et son moteur V6 3,5 L EcoBoost® délivrant une puissance hors normes : tout dans la Ford GT est conçu pour la performance. 
+                    </p>          
+            </div>
+            <div>
+                <img src="{{ asset('/img/images.jpeg') }}" alt="Image" >     
+            </div>  
+        </div>
 
-          </div>
-    </div>
+        <div class="col-lg-4 col-md-5 col-sm-6">
+            <div class="divv">
+                <h2> <br>Badminton</h2>
+                <p>Un nouveau design associant agressivité et efficacité aérodynamique.
+                            Des performances inégalées avec la suspension adaptative pilotée MagneRide™; un plaisir de conduite incomparable grâce au nouvel échappement actif, sublimant les vocalises du mythique V8 
+                </p>
+            </div>
+            <div>
+                <img src="{{ asset('/img/badminton.png') }}" alt="Image">
+            </div> 
+        </div>
+
+
+            
+
+
+
+
+</div>
 @endsection
