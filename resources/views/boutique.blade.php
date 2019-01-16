f6e185c54c2fb3b036d083d614f901f2e66ad425
Sennenryu <troltrol68@hotmail.fr>
2019-01-16 17:20:12 +0100

    Boite a idee

--- resources/views/boutique.blade.php
+++ resources/views/boutique.blade.php
@@ -17,44 +17,75 @@
             </form>
 
         </div>
+    <hr color="darkgrey">
+
     <div class="row1">
         <div class="col-lg-4 col-md-5 col-sm-6">
             <h2><br>Top objet 1</h2>
             <br>
-            <img id="objet1" src="{{ asset('/img/boutique/test.png') }}" alt="Objet1">
+            <img id="topobjet1" src="{{ asset('/img/boutique/test.png') }}" alt="TopObjet1">
+            <h2><br>Prix: 5€</h2>
         </div>
 
         <div class="col-lg-4 col-md-5 col-sm-6">
             <h2><br>Top objet 2</h2>
             <br>
-            <img id="objet2" src="{{ asset('/img/boutique/test.png') }}" alt="Objet2">
+            <img id="topobjet2" src="{{ asset('/img/boutique/test.png') }}" alt="TopObjet2">
+            <h2><br>Prix: 5€</h2>
         </div>
 
-        <div class="col-lg-4 col-md-5 col-sm-6">
+        <div class="col-lg-4 col-md-12 col-sm-12">
             <h2> <br>Top objet 3</h2>
             <br>
-            <img id="objet3" src="{{ asset('/img/boutique/test.png') }}" alt="Objet3">
+            <img id="topobjet3" src="{{ asset('/img/boutique/test.png') }}" alt="TopObjet3">
+            <h2><br>Prix: 5€</h2>
+        </div>
 
     </div>
 </div>
 <div class="container-fluid text-center"> 
     <div class="row">
-        <div class="col-lg-4 col-md-5 col-sm-6 img2">
-            <h2><br>Top objet 1</h2>
+        <div class="col-lg-2 col-md-5 col-sm-4">
+            <h2><br>Objet 1</h2>
             <br>
             <img id="objet1" class=" img2" src="{{ asset('/img/boutique/test.png') }}" alt="Objet1">
+            <h2><br>Prix: 5€</h2>
         </div>
 
-        <div class="col-lg-4 col-md-5 col-sm-6 img2">
-            <h2><br>Top objet 2</h2>
+        <div class="col-lg-2 col-md-5 col-sm-4">
+            <h2><br>Objet 2</h2>
             <br>
             <img id="objet2" class=" img2" src="{{ asset('/img/boutique/test.png') }}" alt="Objet2">
+            <h2><br>Prix: 5€</h2>
         </div>
 
-        <div class="col-lg-4 col-md-5 col-sm-6 img2">
-            <h2> <br>Top objet 3</h2>
+        <div class="col-lg-2 col-md-5 col-sm-4">
+            <h2> <br>Objet 3</h2>
             <br>
             <img id="objet3" class=" img2" src="{{ asset('/img/boutique/test.png') }}" alt="Objet3">
+            <h2><br>Prix: 5€</h2>
+        </div>
+
+        <div class="col-lg-2 col-md-5 col-sm-4">
+            <h2><br>Objet 4</h2>
+            <br>
+            <img id="objet4" class=" img2" src="{{ asset('/img/boutique/test.png') }}" alt="Objet4">
+            <h2><br>Prix: 5€</h2>
+        </div>
+
+        <div class="col-lg-2 col-md-5 col-sm-4">
+            <h2><br>Objet 5</h2>
+            <br>
+            <img id="objet5" class=" img2" src="{{ asset('/img/boutique/test.png') }}" alt="Objet5">
+            <h2><br>Prix: 5€</h2>
+        </div>
+
+        <div class="col-lg-2 col-md-5 col-sm-4">
+            <h2> <br>Objet 6</h2>
+            <br>
+            <img id="objet6" class=" img2" src="{{ asset('/img/boutique/test.png') }}" alt="Objet6">
+            <h2><br>Prix: 5€</h2>
+        </div>
     </div>
 </div>
 @endsection
