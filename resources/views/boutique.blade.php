21ab75e2dea17910e987c888a7a373776c30575a
Sennenryu <troltrol68@hotmail.fr>
2019-01-18 12:07:12 +0100

    Boutique base finie

--- resources/views/boutique.blade.php
+++ resources/views/boutique.blade.php
@@ -5,7 +5,6 @@
 
 <link rel="stylesheet" href="{{ asset('/css/boutique.css') }}">
 
-
 <div id="globalNav" class="container-fluid text-center">
     <h2>
         Panier
@@ -78,12 +77,13 @@
 
         <ul id="onglets">
             <div class = "gauche">
-                <div class="navbar-right dropdown"><a class="dropdown-toggle username" data-toggle="dropdown" aria-expanded="false" href="#"><i class="fas fa-filter img3"></i> <span class="caret"></span></a>
+                <div class="dropdown"><a class="dropdown-toggle username" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" href="#"><i class="fas fa-filter img3"></i> <span class="caret"></span></a>
                     <ul class="dropdown-menu" role="menu">
-                        <li role="presentation"><a href="#">Paramètres</a></li>
-                        <li role="presentation"><a href="#">?</a></li>
+                        <li role="presentation"><a href="#">Nom</a></li>
                         <li role="separator" class="divider"></li>
-                        <li role="presentation"><a href="/deconnexion">Deconnexion</a></li>
+                        <li role="presentation"><a href="/deconnexion">Prix</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li role="presentation"><a href="/deconnexion">Catégorie</a></li>
                     </ul>
                 </div>
             </div>
@@ -94,59 +94,112 @@
         </ul>
     </div>
 
+    <div class="col-lg-12 col-md-12 col-sm-12">
+        <div class = "gauche">
+            <a href="/idees">
+                <i class="fas fa-plus-square"></i>
+            </a>
+        </div>
+    </div>
 
     <div class="row1">
 
-        <a href="/idees">
-            <div class="col-lg-2 col-md-5 col-sm-4 produit">
-                <p>Objet 1</p>
-                <img id="objet1" class=" img2" src="{{ asset('/img/boutique/test.png') }}" alt="Objet1">
-                <p>Prix: 5€</p>
-            </div>
-        </a>
+        <div class="col-lg-2 col-md-5 col-sm-4 ">
+            <a href="/idees">
+                <div class="produit">
+                    <p>Objet 1</p>
+                    <img id="objet1" class=" img2" src="{{ asset('/img/boutique/test.png') }}" alt="Objet1">
+                    <p>Prix: 5€</p>
+                </div>
+            </a>
+            <a href="/idees">
+                <i class="fas fa-edit"></i>
+            </a>
+            <a href="/idees">
+                <i class="fas fa-trash-alt"></i>
+            </a>
+        </div>
 
-        <a href="/idees">
-            <div class="col-lg-2 col-md-5 col-sm-4 produit">
-                <p>Objet 2</p>
-                <img id="objet2" class=" img2" src="{{ asset('/img/boutique/test.png') }}" alt="Objet2">
-                <p>Prix: 5€</p>
-            </div>
-        </a>
+        <div class="col-lg-2 col-md-5 col-sm-4 ">
+            <a href="/idees">
+                <div class="produit">
+                    <p>Objet 2</p>
+                    <img id="objet2" class=" img2" src="{{ asset('/img/boutique/test.png') }}" alt="Objet2">
+                    <p>Prix: 5€</p>
+                </div>
+            </a>
+            <a href="/idees">
+                <i class="fas fa-edit"></i>
+            </a>
+            <a href="/idees">
+                <i class="fas fa-trash-alt"></i>
+            </a>
+        </div>
 
-        <a href="/idees">
-            <div class="col-lg-2 col-md-5 col-sm-4 produit">
-                <p>Objet 3</p>
-                <img id="objet3" class=" img2" src="{{ asset('/img/boutique/test.png') }}" alt="Objet3">
-                <p>Prix: 5€</p>
-            </div>
-        </a>
+        <div class="col-lg-2 col-md-5 col-sm-4 ">
+            <a href="/idees">
+                <div class="produit">
+                    <p>Objet 3</p>
+                    <img id="objet3" class=" img2" src="{{ asset('/img/boutique/test.png') }}" alt="Objet3">
+                    <p>Prix: 5€</p>
+                </div>
+            </a>
+            <a href="/idees">
+                <i class="fas fa-edit"></i>
+            </a>
+            <a href="/idees">
+                <i class="fas fa-trash-alt"></i>
+            </a>
+        </div>
 
-        <a href="/idees">
-            <div class="col-lg-2 col-md-5 col-sm-4 produit">
-                <p>Objet 4</p>
-                <img id="objet4" class=" img2" src="{{ asset('/img/boutique/test.png') }}" alt="Objet4">
-                <p>Prix: 5€</p>
-            </div>
-        </a>
+        <div class="col-lg-2 col-md-5 col-sm-4 ">
+            <a href="/idees">
+                <div class="produit">
+                    <p>Objet 4</p>
+                    <img id="objet4" class=" img2" src="{{ asset('/img/boutique/test.png') }}" alt="Objet4">
+                    <p>Prix: 5€</p>
+                </div>
+            </a>
+            <a href="/idees">
+                <i class="fas fa-edit"></i>
+            </a>
+            <a href="/idees">
+                <i class="fas fa-trash-alt"></i>
+            </a>
+        </div>
 
-        <a href="/idees">
-            <div class="col-lg-2 col-md-5 col-sm-4 produit">
-                <p>Objet 5</p>
-                <img id="objet5" class=" img2" src="{{ asset('/img/boutique/test.png') }}" alt="Objet5">
-                <p>Prix: 5€</p>
-            </div>
-        </a>
+        <div class="col-lg-2 col-md-5 col-sm-4 ">
+            <a href="/idees">
+                <div class="produit">
+                    <p>Objet 5</p>
+                    <img id="objet5" class=" img2" src="{{ asset('/img/boutique/test.png') }}" alt="Objet5">
+                    <p>Prix: 5€</p>
+                </div>
+            </a>
+            <a href="/idees">
+                <i class="fas fa-edit"></i>
+            </a>
+            <a href="/idees">
+                <i class="fas fa-trash-alt"></i>
+            </a>
+        </div>
 
-        <a href="/idees">
-            <div class="col-lg-2 col-md-5 col-sm-4 produit">
-                <p>Objet 6</p>
-                <img id="objet6" class=" img2" src="{{ asset('/img/boutique/test.png') }}" alt="Objet6">
-                <p>Prix: 5€</p>
-            </div>
-        </a>
+        <div class="col-lg-2 col-md-5 col-sm-4 ">
+            <a href="/idees">
+                <div class="produit">
+                    <p>Objet 6</p>
+                    <img id="objet6" class=" img2" src="{{ asset('/img/boutique/test.png') }}" alt="Objet6">
+                    <p>Prix: 5€</p>
+                </div>
+            </a>
+            <a href="/idees">
+                <i class="fas fa-edit"></i>
+            </a>
+            <a href="/idees">
+                <i class="fas fa-trash-alt"></i>
+            </a>
+        </div>
     </div>
-
-
 </div>
 
 
