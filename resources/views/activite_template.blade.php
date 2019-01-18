7a0a3b19ca4ca1e17791fe8368663164ad54f9a8
Bada67 <guillaume.pires@viacesi.fr>
2019-01-18 09:40:25 +0100

    premier bouton activité_template

--- resources/views/activite_template.blade.php
+++ resources/views/activite_template.blade.php
@@ -12,6 +12,7 @@
     <h1> Le nom de l'activité </h1>
     <hr class="hr2">
         <div>
+        <a class="btn btn-default action-button butt" role="button" href="/inscription">Inscription</a>
             <div>
                 <h2><br>Football</h2>   
                 <p>L'innovation. Voilà ce qui caractérise la Ford GT. Sa forme aérodynamique, ses renforts multifonction et son moteur V6 3,5 L EcoBoost® délivrant une puissance hors normes : tout dans la Ford GT est conçu pour la performance.
