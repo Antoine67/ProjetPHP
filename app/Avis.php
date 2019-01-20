e8f69f096ec8e11d4302aa517bcf3c32ec68347e
AntoienM <33087066+AntoienM@users.noreply.github.com>
2019-01-20 22:09:55 +0100

    avis

--- app/Avis.php
+++ app/Avis.php
@@ -7,10 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Avis extends Model
 {
     public $timestamps = false;
-    protected $dateFormat = "Y-m-d H:i:s";
     protected $fillable = ['Contenu','Date_creation','ID_Utilisateurs','ID_Activites'];
 
-    protected $casts = [
-        'Date_creation' => 'date:hh:mm'
-    ];
+    
 }
