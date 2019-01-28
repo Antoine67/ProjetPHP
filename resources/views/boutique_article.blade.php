39b28c2aa25410db2044b5f16d5d17da2bc3e6c0
AntoienM <33087066+AntoienM@users.noreply.github.com>
2019-01-28 16:39:09 +0100

    btique

--- resources/views/boutique_article.blade.php
+++ resources/views/boutique_article.blade.php
@@ -26,7 +26,7 @@ $url=$url . '/';
 
 $article = Article::where('ID',$id_article)->get()[0];
 
-if($article || sizeof($article) <=0) {
+if(!$article || sizeof($article) <=0) {
     echo 'Article non trouvé :(';
 }else {
     if($article['Stock'] == 0) {
