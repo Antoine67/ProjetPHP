ab99922db8333fb3c939219821bcd4cfb685a8ea
Sennenryu <troltrol68@hotmail.fr>
2019-01-29 17:15:21 +0100

    oui

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -71,7 +71,7 @@ function getURL() {
         <div class="header">
             <nav class="navbar navbar-default navigation-clean-search">
                 <div class="container">
-                    <div class="navbar-header"><a class="navbar-brand navbar-link" href="/">BDE</a>
+                    <div class="navbar-header"><a class="navbar-brand navbar-link" href="/"><img class="carousselhove" alt="CarousselElement 1" src="{{ asset('/img/logo.png') }}"></a>
                         <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Naviguer</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
                     </div>
                     <div class="collapse navbar-collapse" id="navcol-1">
